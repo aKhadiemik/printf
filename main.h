86dbb77ceacfb5dbb1ae2521db00fed84c5ad563
Ngarotosto <coagengo@gmail.com>
2023-03-27 08:28:14 -0700

    fix: inclusion of custom putchar prototype in main header file

--- main.h
+++ main.h
@@ -17,6 +17,7 @@ typedef struct specifier_func
     int (*f)(va_list, char *, int, int);
 } specifier_func_t;
 
+int _putchar(char character);
 int _printf(const char *format, ...);
 int print_char(va_list args, char *buffer, int flags, int width);
 int print_string(va_list args, char *buffer, int flags, int width);
