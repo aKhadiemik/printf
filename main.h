bb4d97627034893564da0a7f4e6fbfdc31313ae6
Ngarotosto <coagengo@gmail.com>
2023-03-27 06:27:15 -0700

    feat: initial commit of main header with prototypes for conversion specifiers commented out except d i & %

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+#define BUFFSIZE 1024
+
+/**
+ * struct specifier_func - Struct to specify the function to use
+ * for a given conversion specifier
+ * @specifier: The conversion specifier character
+ * @f: The function to use for the specifier
+ */
+typedef struct specifier_func
+{
+    char specifier;
+    int (*f)(va_list, char *, int, int);
+} specifier_func_t;
+
+int _printf(const char *format, ...);
+int print_char(va_list args, char *buffer, int flags, int width);
+int print_string(va_list args, char *buffer, int flags, int width);
+int print_percent(va_list args, char *buffer, int flags, int width);
+/**
+ * int print_int(va_list args, char *buffer, int flags, int width);
+ * int print_unsigned(va_list args, char *buffer, int flags, int width);
+ * int print_binary(va_list args, char *buffer, int flags, int width);
+ * int print_octal(va_list args, char *buffer, int flags, int width);
+ * int print_hex(va_list args, char *buffer, int flags, int width);
+ * int print_HEX(va_list args, char *buffer, int flags, int width);
+ * int print_rot13(va_list args, char *buffer, int flags, int width);
+ */
+
+int handle_format(const char *format, va_list args, char *buffer, int buffer_index);
+
+#endif /* MAIN_H */
