bc82de2ea76d47bc6be7016634661e99ca29a6d9
Ngarotosto <coagengo@gmail.com>
2023-03-28 05:09:31 -0700

    fix: digit prototype changed to print_digit

---
+++ print_digit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * digit - function top print digit
+ * @d: digit to be printed
+ * @count: lenght count of current string
+ * Return: len of digit
+ */
+int print_digit(int d, int count)
+{
+	int rev_d;
+
+	if (d < 0)
+	{
+		_putchar('-');
+		count++;
+		d = -d;
+	}
+	if (d == 0)
+	{
+		_putchar('0');
+		count++;
+		return (count);
+	}
+	if (d > 0)
+	{
+		rev_d = 0;
+		while (d > 0)
+		{
+			rev_d = rev_d * 10 + (d % 10);
+			d = d / 10;
+		}
+		while (rev_d > 0)
+		{
+			_putchar(rev_d % 10 + '0');
+			count++;
+			rev_d = rev_d / 10;
+		}
+	}
+	return (count);
+}
